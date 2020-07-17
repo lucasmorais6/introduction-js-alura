c3df4e388c21dffdc4b4211086cbdf0453301abf
Lucas Morais <morais56.lucas@gmail.com>
2020-07-17 20:34:21 -0300

    introduction

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector(".titulo") //buscar um elemento dentro do document
+      console.log(titulo); 
+			console.log(titulo.textContent); //apenas o conteudo de texto
+      titulo.textContent = "Novo nome"; //alterar o "valor" da variavel
+ 
